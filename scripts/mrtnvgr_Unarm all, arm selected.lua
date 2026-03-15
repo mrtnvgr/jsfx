6a02418d84dd74e054748aff7e84282acfb5d46d
mrtnvgr <martynovegorOF@yandex.ru>
2026-03-15 20:37:17 +0700

    fix(scripts/`Unarm all, arm selected`)!: use better actions (SWS required now)

--- scripts/mrtnvgr_Unarm all, arm selected.lua
+++ scripts/mrtnvgr_Unarm all, arm selected.lua
@@ -1,11 +1,16 @@
 -- Idea by DawnShiftAudio
 
+reaper.PreventUIRefresh(1);
+
 reaper.Undo_BeginBlock();
 
 -- Track: Unarm all tracks for recording
 reaper.Main_OnCommand(40491, 0);
 
--- Track: Toggle record arm for selected tracks
-reaper.Main_OnCommand(9, 0);
+-- Xenakios/SWS: Set selected tracks record armed
+local arm = reaper.NamedCommandLookup("_XENAKIOS_SELTRAX_RECARMED");
+reaper.Main_OnCommand(arm, 0);
 
 reaper.Undo_EndBlock("Unarm all, arm selected", -1);
+
+reaper.PreventUIRefresh(-1);
