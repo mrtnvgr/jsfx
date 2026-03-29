7d6ca73b4a901164a208106522e7cb0e531c316e
mrtnvgr <martynovegorOF@yandex.ru>
2026-03-29 13:24:40 +0700

    feat(scripts/`Unarm all, arm selected`): make it togglable

--- scripts/mrtnvgr_Unarm all, arm selected.lua
+++ scripts/mrtnvgr_Unarm all, arm selected.lua
@@ -1,16 +1,42 @@
 -- Idea by DawnShiftAudio
 
-reaper.PreventUIRefresh(1);
+NUM_SELECTED = reaper.CountSelectedTracks(0);
 
+function unarmAll()
+    -- Track: Unarm all tracks for recording
+    reaper.Main_OnCommand(40491, 0);
+end
+
+function areAllSelectedTracksArmed()
+    all_armed = true;
+
+    for i = 0, NUM_SELECTED - 1 do
+        local track = reaper.GetSelectedTrack(0, i);
+        local armed = reaper.GetMediaTrackInfo_Value(track, "I_RECARM");
+
+        if armed == 0 then
+            all_armed = false;
+            break;
+        end
+    end
+
+    return all_armed;
+end
+
+reaper.PreventUIRefresh(1);
 reaper.Undo_BeginBlock();
 
--- Track: Unarm all tracks for recording
-reaper.Main_OnCommand(40491, 0);
+if NUM_SELECTED == 0 then
+    unarmAll();
+else
+    local all_armed = areAllSelectedTracksArmed();
+    local recflag = all_armed and 0 or 1;
 
--- Xenakios/SWS: Set selected tracks record armed
-local arm = reaper.NamedCommandLookup("_XENAKIOS_SELTRAX_RECARMED");
-reaper.Main_OnCommand(arm, 0);
+    for i = 0, NUM_SELECTED - 1 do
+        local track = reaper.GetSelectedTrack(0, i);
+        reaper.SetMediaTrackInfo_Value(track, "I_RECARM", recflag);
+    end
+end
 
 reaper.Undo_EndBlock("Unarm all, arm selected", -1);
-
 reaper.PreventUIRefresh(-1);
